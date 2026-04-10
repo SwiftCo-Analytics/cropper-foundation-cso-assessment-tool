51c0058e8eeb41f7733a57b2efd1ac5c3a8aef82
Duane Swift <duanerichardswift@gmail.com>
2026-04-10 17:19:08 -0400

    linter fixes

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,14 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { defineConfig } from "eslint/config";
+import nextCoreWebVitals from "eslint-config-next/core-web-vitals";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
-
-export default eslintConfig;
+export default defineConfig([
+  {
+    ignores: ["eslint.config.mjs", "dist/**", "src/generated/**"],
+  },
+  ...nextCoreWebVitals,
+  {
+    rules: {
+      "react/no-unescaped-entities": "warn",
+    },
+  },
+]);
